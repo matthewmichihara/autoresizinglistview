321c53ffdd0728a0346a7ab4ea7b8ca3ae00eb71
Matthew Michihara <matthew.michihara@gmail.com>
2013-07-30 17:52:21 -0700

    Initial commit

---
+++ library/src/main/java/com/fourpool/growinglistview/GrowingListView.java
@@ -0,0 +1,53 @@
+package com.fourpool.growinglistview;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+/**
+ *
+ */
+public class GrowingListView extends ListView {
+    public GrowingListView(Context context) {
+        super(context);
+    }
+
+    public GrowingListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GrowingListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override public void setAdapter(final ListAdapter adapter) {
+        super.setAdapter(adapter);
+
+        adapter.registerDataSetObserver(new DataSetObserver() {
+            @Override public void onChanged() {
+                setHeight();
+            }
+        });
+
+        setHeight();
+    }
+
+    private void setHeight() {
+        ListAdapter adapter = getAdapter();
+        int totalHeight = 0;
+        for (int i = 0; i < adapter.getCount(); i++) {
+            View listItem = adapter.getView(i, null, this);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = this.getLayoutParams();
+        int dividerHeights = getDividerHeight() * (adapter.getCount() == 0 ? 0 : adapter.getCount() - 1);
+        params.height = totalHeight + dividerHeights;
+        setLayoutParams(params);
+    }
+}
---
+++ sample/src/main/java/com/fourpool/growinglistview/sample/MainActivity.java
@@ -0,0 +1,66 @@
+package com.fourpool.growinglistview.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        Button addButton = (Button) findViewById(R.id.add_button);
+        Button removeButton = (Button) findViewById(R.id.remove_button);
+
+        final Random random = new Random();
+        final List<Integer> ints = new ArrayList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            ints.add(random.nextInt());
+        }
+
+        final ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, ints);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                ints.add(random.nextInt());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                ints.remove(0);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
